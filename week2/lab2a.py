e76b324f8a1334a91df8a428aa04a26a809676ac
Diiorio <idiiorio@email.neit.edu>
2022-02-02 10:12:54 -0500

    lab2a

--- week2/lab2a.py
+++ week2/lab2a.py
@@ -12,5 +12,31 @@
 #NOTES: 
 
 #imports------------------------------------------------
+import csv
 #functions----------------------------------------------
 #Main---------------------------------------------------
+
+
+records = 0
+overMax = 0 
+
+print("Room\t\t\t     Max\t     Min\t     Over")
+
+with open("week2\lab2a.csv") as csvfile:
+    file = csv.reader(csvfile)
+    for row in file:
+        room = row[0]
+        max = int(row[1])
+        min = int(row[2])
+        over = 0
+        records += 1
+
+        if min > max:
+            over = min - max
+            print("{0:20}\t{1:8}\t{2:8}\t{3:8}".format(room, max, min, over))
+            overMax += 1
+        
+print("\nProcessed {0:3} records.".format(records))
+print("There are {0:3} rooms over the limit.".format(overMax))
+
+anyKey = input("\nPress the ENTER key to continue . . .")
