fcebdc2a0fd42b2a71700fc122e770c4d81eb1c3
Diiorio <idiiorio@email.neit.edu>
2022-02-02 09:00:31 -0500

    add all basic files and such w2-4

---
+++ week3/lab3a.py
@@ -0,0 +1,16 @@
+#Isaiah Diiorio
+#
+#
+
+#Program Essentials with Python, SE116, and .21
+#PROGRAM PROMPT: 
+
+#VARIABLE DICTIONARY:
+#varName        description of data it will hold
+
+
+#NOTES: 
+
+#imports------------------------------------------------
+#functions----------------------------------------------
+#Main---------------------------------------------------
