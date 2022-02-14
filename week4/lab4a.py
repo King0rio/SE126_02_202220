a467adfa9d5308cf34621bd0d83d3ce576750536
Diiorio <idiiorio@email.neit.edu>
2022-02-14 09:05:56 -0500

    week 6

--- week4/lab4a.py
+++ week4/lab4a.py
@@ -7,10 +7,32 @@
 
 #VARIABLE DICTIONARY:
 #varName        description of data it will hold
-
+#ram            stores all of the ram value's for computers in the csv file 
+#ramCheck       is used to save the number of computers with more than 8GB of ram 
+#row            is used to read one row at a time from file
+#row            represents a row in the csv file
+#col            represents a column in the csv file
+#ram[col]       used to pull data from that colum of the csv file
 
 #NOTES: 
 
 #imports------------------------------------------------
+import csv
 #functions----------------------------------------------
 #Main---------------------------------------------------
+
+ram = []
+ramCheck = 0
+
+with open("week4\lab4a.csv") as csvfile:
+    file = csv.reader(csvfile)
+    for row in file:
+        
+        ram.append(row[3])
+
+
+for col in range(29):
+    if (int(ram[col]) > 8):
+        ramCheck += 1
+
+print("there are {0} Computers that have more than 8GB of ram ".format(ramCheck))
