e7ef3b5a160d7773abbf75b52d663bd1f4c2debc
Diiorio <idiiorio@email.neit.edu>
2022-02-28 08:00:39 -0500

    week 8

---
+++ week8/lab8.py
@@ -0,0 +1,16 @@
+#Isaiah Diiorio
+#
+#
+
+#Intermediate Programming Using Python, SE126, and .02
+#PROGRAM PROMPT: 
+
+#VARIABLE DICTIONARY:
+#varName        description of data it will hold
+
+
+#NOTES: 
+
+#imports------------------------------------------------
+#functions----------------------------------------------
+#Main---------------------------------------------------
