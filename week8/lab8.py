84dc49c96ee89b5a2cdf036fcd5a8fb38a45f662
Diiorio <idiiorio@email.neit.edu>
2022-03-01 15:34:55 -0500

    yo

--- week8/lab8.py
+++ week8/lab8.py
@@ -20,32 +20,126 @@ def fillRows():
     return x
 
 def seatMap():
-    '''Displays the '''
+    '''Displays the seating in the theater'''
     
     print("Row  A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 1 2 3 4")
     for col in range(1,16):
         print(" {0:2}  {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} {13} {14} {15} {16} {17} {18} {19} {20} {21} {22} {23} {24} {25} {26} {27} {28} {29} {30}".format(col, cA[col], cB[col], cC[col], cD[col], cE[col], cF[col], cG[col], cH[col], cI[col], cJ[col], cK[col], cL[col], cM[col], cN[col], cO[col], cP[col], cQ[col], cR[col], cS[col], cT[col], cU[col], cV[col], cW[col], cX[col], cY[col], cZ[col], c1[col], c2[col], c3[col], c4[col]))
 
-def inputTest1(input1):
-    '''yo'''
-
-def inputTest2(input2):
-    '''yo'''
-
 def seatChoice():
     '''yo'''
+    userInput1 = input("What Row Would You Like Your Seat To Be In : ")
+    userInput2 = input("What Column Would You Like To Be Seated In : ")
 
-def seatsTaken():
+def seatsTaken(column):
     '''yo'''
-    
     seatsTaken = 0
     seatsAvailable = 0
-    this = [[seatsTaken, seatsAvailable], []]
     for row in range(1, 31):
-        if cA[row] == "X":
+        if column[row] == "X":
             seatsTaken += 1
-        elif cA[row] == "#":
+        elif column[row] == "#":
             seatsAvailable += 1
+    
+    return seatsTaken, seatsAvailable
+
+def seatsTakenTotal():
+    '''yo'''
+    seatsTakenTotal = 0
+    seatsAvailableTotal = 0
+
+    temp1, temp2 = seatsTaken(cA)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cB)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cC)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cD)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cE)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cF)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cG)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cH)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cI)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cJ)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cK)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cL)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cM)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cN)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cO)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cP)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cQ)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cR)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cS)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cT)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cU)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cV)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cW)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cX)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cY)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(cZ)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(c1)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(c2)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(c3)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+    temp1, temp2 = seatsTaken(c4)
+    seatsTakenTotal += temp1
+    seatsAvailableTotal += temp2
+
+    return seatsTakenTotal, seatsAvailableTotal
 
 #Main---------------------------------------------------
 
